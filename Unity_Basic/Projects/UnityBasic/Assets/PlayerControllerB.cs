6a65ad5e91eca5d1ee5c09b5d1c4f389a8073bb3
KRJaeh0 <jaeho6627@kakao.com>
2025-03-19 08:12:37 +0900

    씬 메타 파일 및 설정 추가: PlayerController 및 PlayerControllerB 클래스 생성, SceneTemplateSettings.json 추가

---
+++ Unity_Basic/Projects/UnityBasic/Assets/PlayerControllerB.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerB : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World! - B Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
