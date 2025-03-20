025538db58373018094e9da32ab50bdd4fc00542
KRJaeh0 <jaeho6627@kakao.com>
2025-03-20 16:04:27 +0900

    적 프리팹 및 플레이어 프리팹의 레이어 수정: Enemy.prefab에서 레이어를 0에서 7로, Player.prefab에서 0에서 6으로 변경. EnemyController 스크립트에 레이어 마스크 추가 및 레이캐스트 기능 개선.

--- Unity_Basic/Projects/UnityBasic/Assets/Script/EnemyController.cs
+++ Unity_Basic/Projects/UnityBasic/Assets/Script/EnemyController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+// using System.Numerics;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
@@ -9,17 +10,60 @@ public class EnemyController : MonoBehaviour
     // Start is called before the first frame update
 
     // private PlayerController playerController; // cache
+
+    [SerializeField] private LayerMask layerMask; // 레이어 마스크
+
     void Start()
     {
         // playerController = FindAnyObjectByType<PlayerController>(); // PlayerController를 찾아서 가져온다.
 
         // Debug.Log(playerController.gameObject.name);
+
+        InitLayerMask();
+    }
+
+    private void InitLayerMask()
+    {
+        layerMask = 1 << LayerMask.NameToLayer("Player"); // 레이어 마스크를 설정한다. // 비트연산자로 레이어를 설정할 수 있다.
+
+        // layerMask = 1 << 6;              // 0000 0000 0000 0000 0000 0000 0010 0000
+        // layerMask = 1 << 0 | 1 << 5;     // 0000 0000 0000 0000 0000 0000 0001 0001
     }
 
     // Update is called once per frame
     void Update()
     {
-        #region Use Cache
+        // LookAtPlayerUseCache();
+        // LookAtPlayerUsePhysics();
+
+
+        RotateWithRaycast();
+
+    }
+
+    private void RotateWithRaycast()
+    {
+        // gameObject.transform.rotation = Vector3.zero;
+        // 회전값은 quaternion으로 정의하기 때문에 Vector 를 사용할 수 없다.
+        gameObject.transform.Rotate(new Vector3(0, 5, 0));  // gameObject.transform.rotation *= Quaternion.Euler(new Vector3(0, 5, 0)); 
+                                                            // Euler를 이용해 회전값을 vector 값을 이용해 설정할 수 있다. 
+                                                            // * 연산으로 회전값을 누적시켜 회전 시킬 수 있다.
+
+        // raycast 를 활용해 감시하다가 플레이어를 발견하면 경고
+        // if (Physics.Raycast(gameObject.transform.position, gameObject.transform.forward, out RaycastHit hitInfo))
+        if (Physics.Raycast(gameObject.transform.position, gameObject.transform.forward, out RaycastHit hitInfo, 10f, layerMask)) // LayerMask를 이용해 검사할 레이어를 설정할 수 있다. (최적화 방법)
+        {
+            if (hitInfo.collider.gameObject.tag == "Player")
+            {
+                Debug.Log("Hit! " + hitInfo.collider.gameObject.name);
+                Debug.DrawLine(gameObject.transform.position, hitInfo.point, Color.red);    // 레이를 그려준다.
+                hitInfo.collider.GetComponent<PlayerController>().Warning();
+            }
+        }
+    }
+
+    private void LookAtPlayerUseCache()
+    {
         // float fDistance = Vector3.Distance(gameObject.transform.position, playerController.transform.position); // 플레이어와의 거리
         // // Debug.Log(fDistance);
 
@@ -29,10 +73,12 @@ public class EnemyController : MonoBehaviour
         //     gameObject.transform.LookAt(playerController.transform); // 플레이어를 바라보게 한다.
         //     playerController.Warning();     // 플레이어에게 경고한다.
         // }
-        #endregion
+    }
 
+    private void LookAtPlayerUsePhysics()
+    {
         Collider[] colliders = Physics.OverlapSphere(gameObject.transform.position, 5f); // 반경 5f 안에 있는 모든 콜라이더를 반환한다.
-        foreach(Collider collider in colliders)
+        foreach (Collider collider in colliders)
         {
             if (collider.gameObject.tag == "Player")
             {
