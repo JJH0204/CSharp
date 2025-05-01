6158aafc62f0ec3b62d3ec7af12a9b47c8119324
KRJaeh0 <jaeho6627@kakao.com>
2025-05-01 11:01:26 +0900

    EP1: Singleton 패턴을 적용하여 SystemManager의 인스턴스를 관리하고 InitScene_Init에서 메니저 초기화 방식을 개선함

--- Unity_Basic/Projects/UnityPro/Assets/EP1/Script/InitScene/InitScene_Init.cs
+++ Unity_Basic/Projects/UnityPro/Assets/EP1/Script/InitScene/InitScene_Init.cs
@@ -7,34 +7,50 @@ using UnityEngine.SceneManagement;
 
 public class InitScene_Init : MonoBehaviour
 {
+    private bool isInit = false;    // 초기화가 완료되었는지 여부를 나타내는 변수
     private const int PROGRESS_VALUE = 5;
     private int progressAddValue = 0;    // 초기화 진행률을 나타내는 변수
-    private SystemManager systemManager;    // cache
-    private EffectManager effectManager;    // cache
-    private ObjPoolManager objPoolManager;  // cache
-    private WindowManager windowManager;    // cache
-    private SoundManager soundManager;      // cache
+    // private SystemManager systemManager;    // cache
+    // private EffectManager effectManager;    // cache
+    // private ObjPoolManager objPoolManager;  // cache
+    // private WindowManager windowManager;    // cache
+    // private SoundManager soundManager;      // cache
     private InitScene_UI initSceneUI;       // cache
 
     private void Awake()    // Scene이 로드되어 오브젝트들의 인스턴스화가 완료되면 호출되어 초기화에 필요한 메니저들을 캐싱한다.
     {
+        initSceneUI = FindAnyObjectByType<InitScene_UI>();
+
+        // if (!isInit)    // 초기화가 완료되지 않았다면
+        // {
+        //     isInit = true;
+        //     // 메니저 객체 인스턴스를 생성성
+        //     systemManager = new GameObject("SystemManager").AddComponent<SystemManager>();
+        //     effectManager = new GameObject("EffectManager").AddComponent<EffectManager>();
+        //     objPoolManager = new GameObject("ObjPoolManager").AddComponent<ObjPoolManager>();
+        //     windowManager = new GameObject("WindowManager").AddComponent<WindowManager>();
+        //     soundManager = new GameObject("SoundManager").AddComponent<SoundManager>();
+        // }
+        // else    // 초기화가 완료되었다면
+        // {
+        //     systemManager = FindAnyObjectByType<SystemManager>();
+        //     effectManager = FindAnyObjectByType<EffectManager>();
+        //     objPoolManager = FindAnyObjectByType<ObjPoolManager>();
+        //     windowManager = FindAnyObjectByType<WindowManager>();
+        //     soundManager = FindAnyObjectByType<SoundManager>();
+        //     initSceneUI = FindAnyObjectByType<InitScene_UI>();
+        // }
+    }
+
+    private void Start()    // Awake()가 끝나고 호출되어 초기화가 필요한 메니저들을 일괄 초기화 시킨다.
+    {
+        // yield return null;  // 다음 프레임까지 대기하여 모든 오브젝트들이 초기화된 후에 메니저들을 캐싱한다.
         // systemManager = FindAnyObjectByType<SystemManager>();
         // effectManager = FindAnyObjectByType<EffectManager>();
         // objPoolManager = FindAnyObjectByType<ObjPoolManager>();
         // windowManager = FindAnyObjectByType<WindowManager>();
         // soundManager = FindAnyObjectByType<SoundManager>();
         // initSceneUI = FindAnyObjectByType<InitScene_UI>();
-    }
-
-    private IEnumerator Start()    // Awake()가 끝나고 호출되어 초기화가 필요한 메니저들을 일괄 초기화 시킨다.
-    {
-        yield return null;  // 다음 프레임까지 대기하여 모든 오브젝트들이 초기화된 후에 메니저들을 캐싱한다.
-        systemManager = FindAnyObjectByType<SystemManager>();
-        effectManager = FindAnyObjectByType<EffectManager>();
-        objPoolManager = FindAnyObjectByType<ObjPoolManager>();
-        windowManager = FindAnyObjectByType<WindowManager>();
-        soundManager = FindAnyObjectByType<SoundManager>();
-        initSceneUI = FindAnyObjectByType<InitScene_UI>();
 
         StartCoroutine(C_Manager());    // 초기화 메니저 코루틴 시작
     }
@@ -82,27 +98,30 @@ public class InitScene_Init : MonoBehaviour
     // 초기화에 필요한 동작들 정의
     private void SystemManagerInit()
     {
-        systemManager.SetInit();
+        // systemManager.SetInit();
+
+        // Singleton 패턴으로 캐싱 없이 SystemManager 인스턴스에 접근하여 초기화
+        SystemManager.Instance.SetInit();    // SystemManager의 SetInit() 메서드 호출
     }
 
     private void EffectManagerInit()
     {
-        effectManager.SetInit();
+        // effectManager.SetInit();
     }
 
     private void ObjPoolManagerInit()
     {
-        objPoolManager.SetInit();
+        // objPoolManager.SetInit();
     }
 
     private void WindowManagerInit()
     {
-        windowManager.SetInit();
+        // windowManager.SetInit();
     }
 
     private void SoundManagerInit()
     {
-        soundManager.SetInit();
+        // soundManager.SetInit();
     }
 
     private void LoadScene()
--- Unity_Basic/Projects/UnityPro/Assets/EP1/Script/Manager/SystemManager.cs
+++ Unity_Basic/Projects/UnityPro/Assets/EP1/Script/Manager/SystemManager.cs
@@ -5,6 +5,27 @@ using UnityEngine;
 
 public class SystemManager : ManagerBase
 {
+    // Singleton
+    // 오브젝트가 씬에 하나만 존재하도록 보장하는 패턴
+    private static SystemManager instance = null;
+
+    public static SystemManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<SystemManager>();
+                if (instance == null)
+                {
+                    GameObject obj = new GameObject("SystemManager");
+                    instance = obj.AddComponent<SystemManager>();
+                }
+            }
+            return instance;
+        }
+    }
+
     public bool IsInit { get; set; } = false;
 
     private void Awake()
