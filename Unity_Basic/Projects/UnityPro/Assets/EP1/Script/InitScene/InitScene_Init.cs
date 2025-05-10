7d105a1c5b27a01177390c192b5f5aa6a7b308e3
KRJaeh0 <145985933+JJH0204@users.noreply.github.com>
2025-05-10 13:07:53 +0900

    EP1: 네트워크 요청 처리 방식 수정(Call-back)

--- Unity_Basic/Projects/UnityPro/Assets/EP1/Script/InitScene/InitScene_Init.cs
+++ Unity_Basic/Projects/UnityPro/Assets/EP1/Script/InitScene/InitScene_Init.cs
@@ -8,56 +8,27 @@ using UnityEngine.SceneManagement;
 
 public class InitScene_Init : MonoBehaviour
 {
-    // private bool isInit = false;    // 초기화가 완료되었는지 여부를 나타내는 변수
     private const int PROGRESS_VALUE = 4;
     private int progressAddValue = 0;    // 초기화 진행률을 나타내는 변수
-    // private SystemManager systemManager;    // cache
-    // private EffectManager effectManager;    // cache
-    // private ObjPoolManager objPoolManager;  // cache
-    // private WindowManager windowManager;    // cache
-    // private SoundManager soundManager;      // cache
     private InitScene_UI initSceneUI;       // cache
 
     private void Awake()    // Scene이 로드되어 오브젝트들의 인스턴스화가 완료되면 호출되어 초기화에 필요한 메니저들을 캐싱한다.
     {
         initSceneUI = FindAnyObjectByType<InitScene_UI>();
-
-        // if (!isInit)    // 초기화가 완료되지 않았다면
-        // {
-        //     isInit = true;
-        //     // 메니저 객체 인스턴스를 생성성
-        //     systemManager = new GameObject("SystemManager").AddComponent<SystemManager>();
-        //     effectManager = new GameObject("EffectManager").AddComponent<EffectManager>();
-        //     objPoolManager = new GameObject("ObjPoolManager").AddComponent<ObjPoolManager>();
-        //     windowManager = new GameObject("WindowManager").AddComponent<WindowManager>();
-        //     soundManager = new GameObject("SoundManager").AddComponent<SoundManager>();
-        // }
-        // else    // 초기화가 완료되었다면
-        // {
-        //     systemManager = FindAnyObjectByType<SystemManager>();
-        //     effectManager = FindAnyObjectByType<EffectManager>();
-        //     objPoolManager = FindAnyObjectByType<ObjPoolManager>();
-        //     windowManager = FindAnyObjectByType<WindowManager>();
-        //     soundManager = FindAnyObjectByType<SoundManager>();
-        //     initSceneUI = FindAnyObjectByType<InitScene_UI>();
-        // }
     }
 
     private void Start()    // Awake()가 끝나고 호출되어 초기화가 필요한 메니저들을 일괄 초기화 시킨다.
     {
-        // yield return null;  // 다음 프레임까지 대기하여 모든 오브젝트들이 초기화된 후에 메니저들을 캐싱한다.
-        // systemManager = FindAnyObjectByType<SystemManager>();
-        // effectManager = FindAnyObjectByType<EffectManager>();
-        // objPoolManager = FindAnyObjectByType<ObjPoolManager>();
-        // windowManager = FindAnyObjectByType<WindowManager>();
-        // soundManager = FindAnyObjectByType<SoundManager>();
-        // initSceneUI = FindAnyObjectByType<InitScene_UI>();
-
         StartCoroutine(C_Manager());    // 초기화 메니저 코루틴 시작
     }
 
     private IEnumerator C_Manager()
     {
+# if By_Call_Back
+        NetworkManagerInit();
+        yield return new WaitForSeconds(0.1f);
+        SetProgress();
+#else
         IEnumerator enumerator = NetworkManagerInit();    // 네트워크 초기화
         yield return StartCoroutine(enumerator);
         bool isNetworkInitSuccess = (bool)enumerator.Current;    // 네트워크 초기화 성공 여부
@@ -71,38 +42,8 @@ public class InitScene_Init : MonoBehaviour
             yield break;    // 초기화 실패 시 코루틴 종료
         }
 
-        List<Action> actions = new List<Action>()
-        {
-            SystemManagerInit,
-            // EffectManagerInit,
-            // ObjPoolManagerInit,
-            // WindowManagerInit,
-            // SoundManagerInit,
-            SceneLoadManagerInit,
-            LoadScene
-        };
-
-        foreach (var action in actions)
-        {
-            yield return new WaitForSeconds(0.1f);    // 0.1초 대기
-            action?.Invoke();    // 각 초기화 메서드 호출
-            SetProgress();    // 진행률 UI 업데이트
-        }
-        // SystemManagerInit();
-        // SetProgress();
-        // yield return new WaitForSeconds(0.1f);    // 0.1초 대기
-        // EffectManagerInit();
-        // SetProgress();
-        // yield return new WaitForSeconds(0.1f);    // 0.1초 대기
-        // ObjPoolManagerInit();
-        // SetProgress();
-        // yield return new WaitForSeconds(0.1f);    // 0.1초 대기
-        // WindowManagerInit();
-        // SetProgress();
-        // yield return new WaitForSeconds(0.1f);    // 0.1초 대기
-        // SoundManagerInit();
-        // SetProgress();
-        // yield return new WaitForSeconds(0.1f);    // 0.1초 대기
+        yield return StartCoroutine(EtcManager());
+#endif
     }
 
     private void SetProgress()
@@ -125,6 +66,35 @@ public class InitScene_Init : MonoBehaviour
     }
 
 
+#if By_Call_Back
+    private void NetworkManagerInit()
+    {
+        NetworkManager.Instance.SetInit(apiUrl: Config.SERVER_APP_CONFIG_URL);    // 서버 API URL을 설정하여 초기화
+
+        ApplicationConfigSendPacket sendPacket = new ApplicationConfigSendPacket(
+            Config.SERVER_APP_CONFIG_URL,
+            PACKET_NAME_TYPE.ApplicationConfig,
+            Config.E_ENVIRONMENT_TYPE,
+            Config.E_OS_TYPE,
+            Config.APP_VERSION);
+        NetworkManager.Instance.C_SendPacket<ApplicationConfigReceivePacket>(sendPacket, AppConfig);    // 서버에 연결 요청
+    }
+
+    private void AppConfig(ReceivePacketBase receivePacketBase)
+    {
+        ApplicationConfigReceivePacket receivePacket = receivePacketBase as ApplicationConfigReceivePacket;
+        if (receivePacket != null && receivePacket.ReturnCode == (int)RETURN_CODE.OK)    // 서버에서 응답이 성공적으로 왔다면
+        {
+            SystemManager.Instance.ApiUrl = receivePacket.ApiUrl;    // 서버에서 내려준 주소를 사용
+            Debug.Log("NetworkManager Init Success");
+            StartCoroutine(EtcManager());
+        }
+        else
+        {
+            Debug.Log("NetworkManager Init Failed"); // 서버 오류, 안내창 띄워주기기
+        }
+    }
+#else
     private IEnumerator NetworkManagerInit()
     {
         NetworkManager.Instance.SetInit(apiUrl: Config.SERVER_APP_CONFIG_URL);    // 서버 API URL을 설정하여 초기화
@@ -135,7 +105,6 @@ public class InitScene_Init : MonoBehaviour
             Config.E_ENVIRONMENT_TYPE,
             Config.E_OS_TYPE,
             Config.APP_VERSION);
-
         IEnumerator coroutine = NetworkManager.Instance.C_SendPacket<ApplicationConfigReceivePacket>(sendPacket);
         StartCoroutine(coroutine);    // 서버에 연결 요청
         ApplicationConfigReceivePacket receivePacket = coroutine.Current as ApplicationConfigReceivePacket;
@@ -151,7 +120,27 @@ public class InitScene_Init : MonoBehaviour
         }
         // NetworkManager.Instance.SendPacket(sendPacket);    // 서버에 연결 요청
     }
+#endif
+    private IEnumerator EtcManager()
+    {
+        List<Action> actions = new List<Action>()
+        {
+            SystemManagerInit,
+            // EffectManagerInit,
+            // ObjPoolManagerInit,
+            // WindowManagerInit,
+            // SoundManagerInit,
+            SceneLoadManagerInit,
+            LoadScene
+        };
 
+        foreach (var action in actions)
+        {
+            yield return new WaitForSeconds(0.1f);    // 0.1초 대기
+            action?.Invoke();    // 각 초기화 메서드 호출
+            SetProgress();    // 진행률 UI 업데이트
+        }
+    }
 
     private void EffectManagerInit()
     {
--- Unity_Basic/Projects/UnityPro/Assets/EP1/Script/Manager/NetworkManager.cs
+++ Unity_Basic/Projects/UnityPro/Assets/EP1/Script/Manager/NetworkManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
-
+using System;
 public class CertHandler : CertificateHandler
 {
     protected override bool ValidateCertificate(byte[] certificateData)
@@ -86,6 +86,51 @@ public class NetworkManager : ManagerBase
 
                 T receivePacket = JsonUtility.FromJson<T>(jsonData);
                 yield return receivePacket;
+
+                // Debug.Log("ReturnCode: " + applicationConfigReceivePacket.ReturnCode);
+                // Debug.Log("ApiUrl: " + applicationConfigReceivePacket.ApiUrl);
+            }
+        }
+    }
+
+
+    // call-back
+    public IEnumerator C_SendPacket<T>(SendPacketBase sendPacket, Action<ReceivePacketBase> action = null) where T : ReceivePacketBase
+    {
+        string packet = JsonUtility.ToJson(sendPacket);
+        Debug.Log("[Send packet]: " + packet);
+
+        // using 으로 UnityWebRequest 객체를 생성, 사용 후 자동으로 해제
+        using (UnityWebRequest request = UnityWebRequest.PostWwwForm(apiUrl, packet))
+        {
+            // http 통신을 위한 POST 요청 생성성
+            byte[] bytes = new System.Text.UTF8Encoding().GetBytes(packet);
+            request.uploadHandler = new UploadHandlerRaw(bytes);
+            request.downloadHandler = new DownloadHandlerBuffer();
+            request.SetRequestHeader("Content-Type", "application/json");
+
+            // HTTPS 요청을 위한 추가 설정: Ignore SSL certificate errors
+            request.certificateHandler = new CertHandler();
+
+            // Debug.Log("Connected to server: " + apiUrl);
+            yield return request.SendWebRequest();
+
+            if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
+            {
+                Debug.LogError("Error: " + request.error);
+                yield return null;
+                action?.Invoke(new ReceivePacketBase((int)RETURN_CODE.ERROR));
+            }
+            else
+            {
+                // Debug.Log("Connected to server: " + apiUrl);
+                // 성공적으로 응답을 받았을 때
+                string jsonData = request.downloadHandler.text;
+                Debug.Log("Received data: " + jsonData);
+
+                T receivePacket = JsonUtility.FromJson<T>(jsonData);
+                yield return receivePacket;
+                action?.Invoke(receivePacket);
 
                 // Debug.Log("ReturnCode: " + applicationConfigReceivePacket.ReturnCode);
                 // Debug.Log("ApiUrl: " + applicationConfigReceivePacket.ApiUrl);
