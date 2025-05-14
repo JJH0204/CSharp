2e47401cbe95582e8630df5ff76120316e59dfee
KRJaeh0 <145985933+JJH0204@users.noreply.github.com>
2025-05-14 22:21:12 +0900

    Mini-Game01: First Commit
    Add initial game management and UI components
    
    - Created GameManager class for managing game state.
    - Implemented ManagerBase class to handle singleton behavior for managers.
    - Added ScoreView and TimeView classes for displaying score and time.
    - Set up project structure with necessary meta files and package dependencies.
    - Configured project settings including version and scene template settings.

---
+++ Unity_MiniGame/Mini-Game01/Assets/_Project/Script/Runtime/Manager/GameManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class GameManager : ManagerBase
+{
+    private static GameManager instance = null;
+
+    [SerializeField]
+    private int nScore = 0;
+    private Timer timer = null;
+    private bool isGameOver = false;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<GameManager>();
+                if (instance == null)
+                {
+                    GameObject obj = new GameObject("GameManager");
+                    instance = obj.AddComponent<GameManager>();
+                }
+            }
+            return instance;
+        }
+    }
+}
---
+++ Unity_MiniGame/Mini-Game01/Assets/_Project/Script/Runtime/Manager/ManagerBase.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerBase : MonoBehaviour
+{
+    protected void DontDestroy<T>() where T : Object
+    {
+        T[] managers = FindObjectsByType<T>(FindObjectsSortMode.None);
+
+        if (managers.Length > 1)
+            Destroy(gameObject);
+        else
+            DontDestroyOnLoad(gameObject);
+    }
+}
