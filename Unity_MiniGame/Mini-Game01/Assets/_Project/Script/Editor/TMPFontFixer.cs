9450d0684d0b2d630358e7d5bf8fd53317e53e1b
KRJaeh0 <145985933+JJH0204@users.noreply.github.com>
2025-05-22 21:29:43 +0900

    MiniGame01: FontFixer 구현 및 테스트

--- Unity_MiniGame/Mini-Game01/Assets/_Project/Script/Editor/TMPFontFixer.cs
+++ Unity_MiniGame/Mini-Game01/Assets/_Project/Script/Editor/TMPFontFixer.cs
@@ -1,18 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
+#if UNITY_EDITOR
+using UnityEditor;
 using UnityEngine;
+using TMPro;
 
-public class TMPFontFixer : MonoBehaviour
+public class TMPFontFixer : EditorWindow
 {
-    // Start is called before the first frame update
-    void Start()
+    public TMP_FontAsset targetFont;
+
+    [MenuItem("Tools/TMP Font Fixer")]
+    public static void ShowWindow()
     {
-        
+        GetWindow<TMPFontFixer>("TMP Font Fixer");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnGUI()
     {
-        
+        targetFont = (TMP_FontAsset)EditorGUILayout.ObjectField("Font to Apply", targetFont, typeof(TMP_FontAsset), false);
+
+        if (GUILayout.Button("Apply to All TMP Texts"))
+        {
+            ApplyFontToAllTMPTexts();
+        }
+    }
+
+    private void ApplyFontToAllTMPTexts()
+    {
+        var allTexts = Resources.FindObjectsOfTypeAll<TextMeshProUGUI>();
+        int count = 0;
+
+        foreach (var text in allTexts)
+        {
+            if (text.font != targetFont)
+            {
+                Undo.RecordObject(text, "Change TMP Font");
+                text.font = targetFont;
+                EditorUtility.SetDirty(text);
+                count++;
+            }
+        }
+
+        Debug.Log($"TMP Font Fixer: Updated {count} TMP texts.");
     }
 }
+#endif
